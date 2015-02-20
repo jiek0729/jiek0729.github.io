e8c904351004dce3b05ff50ed47fc9033b59743a
Jie <kangjie112@gmail.com>
2015-02-20 09:24:21 -0600

    mark stations.
    mark stations based on input text. (column positions hard coded)

--- app.js
+++ app.js
@@ -1,102 +1,138 @@
 /**
  * Created by Megagirl on 2/12/15.
  */
-angular.module('myApplicationModule', ['uiGmapgoogle-maps'])
-  .config(function(uiGmapGoogleMapApiProvider) {
-    uiGmapGoogleMapApiProvider.configure({
-      //key: 'AIzaSyBZ9tilsR9M7eN4cx1FWPDAlunq7SRC6mw',
-      v: '3.17',
-      libraries: 'weather,geometry,visualization'
-    });
-  })
-  .controller("someController", function($scope, uiGmapGoogleMapApi) {
-    // Do stuff with your $scope.
-    // Note: Some of the directives require at least something to be defined originally!
-    // e.g. $scope.markers = []
-
-    // uiGmapGoogleMapApi is a promise.
-    // The "then" callback function provides the google.maps object.
-    uiGmapGoogleMapApi.then(function(maps) {
-      $scope.title = "Smartrac Client"
-      $scope.map = {center: {latitude: 40.1451, longitude: -99.6680 }, zoom: 4, bounds: {}};
-      $scope.ps =
-        [{
-          id: 1,
-          path: [
-            {
-              latitude: 45,
-              longitude: -74
-            },
-            {
-              latitude: 30,
-              longitude: -89
-            },
-            {
-              latitude: 37,
-              longitude: -122
-            },
-            {
-              latitude: 60,
-              longitude: -95
-            }
-          ],
-          stroke: {
-            color: '#6060FB',
-            weight: 3
-          },
-          editable: true,
-          draggable: true,
-          geodesic: true,
-          visible: true,
-          icons: [{
-            icon: {
-              path: google.maps.SymbolPath.BACKWARD_OPEN_ARROW
-            },
-            offset: '25px',
-            repeat: '50px'
-          }]
+angular.module('myApplicationModule', ['uiGmapgoogle-maps', 'ngGPlaces'])
+  .constant("polylines", [{
+    id: 1,
+    path: [
+      {
+        latitude: 45,
+        longitude: -74
+      },
+      {
+        latitude: 30,
+        longitude: -89
+      },
+      {
+        latitude: 37,
+        longitude: -122
+      },
+      {
+        latitude: 60,
+        longitude: -95
+      }
+    ],
+    stroke: {
+      color: '#6060FB',
+      weight: 3
+    },
+    editable: true,
+    draggable: true,
+    geodesic: true,
+    visible: true,
+    icons: [{
+      icon: {
+        path: google.maps.SymbolPath.BACKWARD_OPEN_ARROW
+      },
+      offset: '25px',
+      repeat: '50px'
+    }]
+  },
+    {
+      id: 2,
+      path: [
+        {
+          latitude: 47,
+          longitude: -74
         },
         {
-          id: 2,
-          path: [
-            {
-              latitude: 47,
-              longitude: -74
-            },
-            {
-              latitude: 32,
-              longitude: -89
-            },
-            {
-              latitude: 39,
-              longitude: -122
-            },
-            {
-              latitude: 62,
-              longitude: -95
-            }
-          ],
-          stroke: {
-            color: '#6060FB',
-            weight: 3
-          },
-          editable: true,
-          draggable: true,
-          geodesic: true,
-          visible: true,
-          icons: [{
-            icon: {
-              path: google.maps.SymbolPath.BACKWARD_OPEN_ARROW
-            },
-            offset: '25px',
-            repeat: '50px'
-          }]
-        }];
-    });
+          latitude: 32,
+          longitude: -89
+        },
+        {
+          latitude: 39,
+          longitude: -122
+        },
+        {
+          latitude: 62,
+          longitude: -95
+        }
+      ],
+      stroke: {
+        color: '#6060FB',
+        weight: 3
+      },
+      editable: true,
+      draggable: true,
+      geodesic: true,
+      visible: true,
+      icons: [{
+        icon: {
+          path: google.maps.SymbolPath.BACKWARD_OPEN_ARROW
+        },
+        offset: '25px',
+        repeat: '50px'
+      }]
+    }])
+  .controller("someController", function($scope) {
+    function initialize() {
+      var mapOptions = {center: {lat: 40.1451, lng: -99.6680 }, zoom: 4};
+      $scope.map = new google.maps.Map(document.getElementById('map-canvas'),
+        mapOptions);
+    }
+    google.maps.event.addDomListener(window, 'load', initialize);
 
     $scope.showContent = function($fileContent){
       $scope.content = $fileContent;
     };
+
+    $scope.markers = []
+
+    $scope.showContent = function($fileContent){
+      $scope.content = $fileContent;
+    };
+
+    $scope.markerHandler = function(){
+      id = parseInt(this.title);
+
+      var station = $scope.stations[parseInt(this.title)];
+
+      var content = "";
+      for (var property in station) {
+        content += "<hr/>" + property + ": " + station[property];
+      }
+
+      var infowindow = new google.maps.InfoWindow({
+        content: content
+        //content: JSON.stringify(station, null, 2)
+      });
+
+      infowindow.open($scope.map, this);
+      //window.console.log(this);
+    }
+
+    $scope.parse = function() {
+
+      $scope.stations = csvJSON($scope.content);
+
+      for(i in $scope.markers) {
+        $scope.markers[i].setMap(null);
+      }
+
+      $scope.markers=[];
+
+      for (id in $scope.stations) {
+        var marker = new google.maps.Marker({
+          position: $scope.stations[id].coords,
+          map: $scope.map,
+          title: id
+        });
+
+        google.maps.event.addListener(marker, 'click', $scope.markerHandler);
+
+        $scope.markers.push(marker);
+      }
+    }
   })
   .directive('onReadFile', function ($parse) {
     return {
@@ -118,30 +154,4 @@ angular.module('myApplicationModule', ['uiGmapgoogle-maps'])
         });
       }
     };
-  })
-  .controller('MainCtrl', function ($scope) {
-    $scope.showContent = function($fileContent){
-      $scope.content = $fileContent;
-    };
-  })
-  .directive('onReadFile', function ($parse) {
-  return {
-    restrict: 'A',
-    scope: false,
-    link: function(scope, element, attrs) {
-      var fn = $parse(attrs.onReadFile);
-
-      element.on('change', function(onChangeEvent) {
-        var reader = new FileReader();
-
-        reader.onload = function(onLoadEvent) {
-          scope.$apply(function() {
-            fn(scope, {$fileContent:onLoadEvent.target.result});
-          });
-        };
-
-        reader.readAsText((onChangeEvent.srcElement || onChangeEvent.target).files[0]);
-      });
-    }
-  };
-});
+  });
---
+++ model.js
@@ -0,0 +1,58 @@
+function station(params) {
+  this.station_name = params[0];
+  this.coords = new google.maps.LatLng(parseFloat(params[2]), parseFloat(params[1]));
+  this.rail = parseInt(params[3]) == 1 ? true : false;
+  this.system = params[4];
+  this.mode = params[5];
+  this.line = params[6];
+  this.region = params[7];
+  this.state = params[8];
+  this.station_id = parseInt(params[9]);
+  this.city = params[10];
+}
+
+// Return array of string values, or NULL if CSV string not well formed.
+function CSVtoArray(text) {
+  var re_valid = /^\s*(?:'[^'\\]*(?:\\[\S\s][^'\\]*)*'|"[^"\\]*(?:\\[\S\s][^"\\]*)*"|[^,'"\s\\]*(?:\s+[^,'"\s\\]+)*)\s*(?:,\s*(?:'[^'\\]*(?:\\[\S\s][^'\\]*)*'|"[^"\\]*(?:\\[\S\s][^"\\]*)*"|[^,'"\s\\]*(?:\s+[^,'"\s\\]+)*)\s*)*$/;
+  var re_value = /(?!\s*$)\s*(?:'([^'\\]*(?:\\[\S\s][^'\\]*)*)'|"([^"\\]*(?:\\[\S\s][^"\\]*)*)"|([^,'"\s\\]*(?:\s+[^,'"\s\\]+)*))\s*(?:,|$)/g;
+  // Return NULL if input string is not well formed CSV string.
+  if (!re_valid.test(text)) return null;
+  var a = [];                     // Initialize array to receive values.
+  text.replace(re_value, // "Walk" the string using replace with callback.
+    function(m0, m1, m2, m3) {
+      // Remove backslash from \' in single quoted values.
+      if      (m1 !== undefined) a.push(m1.replace(/\\'/g, "'"));
+      // Remove backslash from \" in double quoted values.
+      else if (m2 !== undefined) a.push(m2.replace(/\\"/g, '"'));
+      else if (m3 !== undefined) a.push(m3);
+      return ''; // Return empty string.
+    });
+  // Handle special case of empty last value.
+  if (/,\s*$/.test(text)) a.push('');
+  return a;
+};
+
+function csvJSON(csv){
+
+  var lines=csv.split("\n");
+
+  var result = {};
+
+  var headers=lines[0].split(",");
+
+  for(var i=1;i<lines.length;i++){
+
+    var currentline=CSVtoArray(lines[i]);
+
+    if(currentline === null || currentline.length === 0) {
+      continue;
+    }
+
+    st = new station(currentline);
+
+    result[st.station_id] = st;
+  }
+
+  return result; //JavaScript object
+  //return JSON.stringify(result); //JSON
+}
